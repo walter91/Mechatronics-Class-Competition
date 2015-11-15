7f6939f6f815960e36b510ee48b60ae6212b2457
walter91 <waltermcoe@gmail.com>
2015-11-15 07:59:03 -0700

    Added PWM output to ultra test

--- ultrasonic_testing.X/ultra_main.c
+++ ultrasonic_testing.X/ultra_main.c
@@ -6,16 +6,48 @@ _FOSCSEL (FNOSC_FRCDIV);   //8mHz with Post-scaling
 
 int STATE = 0;
 
+void config_pwm_14()
+{
+    //------------------------------------------------------------------
+    // CONFIGURE PWM1 (on pin 14) USING TIMER2
+
+    // Configure Timer2, which will be the source for the output compare that
+    // drives PWM1
+    T2CONbits.TON = 0;      // Timer2 off while configuring PWM, pg. 144
+    T2CONbits.TCKPS = 0b00;    // Timer2 1:1 clock pre-scale, pg. 144
+    T2CONbits.T32 = 0;      // Timer2 acts as a single 16-bit timer, pg. 144
+    T2CONbits.TCS = 0;      // Timer2 internal clock source, pg. 144
+
+    // Configure PWM1 on OC1 (pin 14)
+    OC1CON1 = 0b0000;               // Clear OC1 configuration bits, Table 4-8
+    OC1CON2 = 0b0000;               // Clear OC1 configuration bits, Table 4-8
+    OC1CON1bits.OCTSEL = 0b000;       // Use Timer2, pg. 157
+    OC1CON1bits.OCM = 0b110;          // Edge-aligned PWM mode, pg. 158
+    OC1CON2bits.SYNCSEL = 0b01100;    // Use Timer2, pg. 160
+
+    // Set period and duty cycle
+    PR2 = 3999;                // Period of Timer2 to achieve FPWM = 1 kHz
+                            // See Equation 15-1 in the data sheet
+    OC1R = 3999;               // Set Output Compare value to achieve desired duty
+                            // cycle. This is the number of timer counts when
+                            // the OC should send the PWM signal low. The duty
+                            // cycle as a fraction is OC1R/PR2.
+
+    // Turn on Timer2
+    T2CONbits.TON = 1;
+}
+
+
 int main()
 {
     pin_config_init();
     timing_interrupt_config();
+    ultrasonic_setup();
     
     
     while(1)
     {
-       switch(STATE)
-       {}            
+        OC1R = 83.3*read_dist();    //Should output PWM with duty cycle from 0%-100% for 0"-48"
     }
 }
 
