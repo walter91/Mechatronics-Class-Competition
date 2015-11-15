a1100bd30c91c2700ee54a8e4ac9f2ed43daa898
walter91 <waltermcoe@gmail.com>
2015-11-15 07:03:50 -0700

    Added blank ultra_testing project

---
+++ ultrasonic_testing.X/ultra_main.c
@@ -0,0 +1,21 @@
+#include <p24f16KA301.h>
+
+_FOSCSEL (FNOSC_FRCDIV);   //8mHz with Post-scaling
+
+#include "C:\Users\Walter\Documents\Walter\School\Fall 2015\Mechatronics\Mechatronics-Class-Competition\competition.X\board.h" //set up pin names, hold all additional functions
+
+int STATE = 0;
+
+int main()
+{
+    pin_config_init();
+    timing_interrupt_config();
+    
+    
+    while(1)
+    {
+       switch(STATE)
+       {}            
+    }
+}
+
---
+++ ultrasonic_testing.X/ultrasonic_main.c
@@ -0,0 +1,12 @@
+#include <p24f16KA301.h>
+
+_FOSCSEL (FNOSC_FRCDIV);   //8mHz with Post-scaling
+
+
+int main()
+{
+    while(1)
+    {
+        
+    }
+}
